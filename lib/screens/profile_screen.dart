4518082f661f31c1eb7160508a4f1f3c58139a3c
AlvindraRamadhan <2300016035@webmail.uad.ac.id>
2025-06-28 18:38:45 +0700

    Penambahan beberapa fitur

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -16,7 +16,7 @@ class ProfileScreen extends StatelessWidget {
               child: CircleAvatar(
                 radius: 50,
                 backgroundImage: NetworkImage(
-                  'https://example.com/profile.jpg',
+                  'https://plus.unsplash.com/premium_photo-1689568126014-06fea9d5d341?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
                 ),
               ),
             ),
